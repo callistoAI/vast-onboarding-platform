6db8d7b35be9dcf3d94ebec640e5fa8be1b17159
Henry Paquin <henry@Henrys-MacBook-Air-3.local>
2025-08-19 13:18:18 +0200

    Update stats card colors, add yellow star icons for standard links, change font to Inter

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,7 @@ export default {
   theme: {
     extend: {
       fontFamily: {
-        sans: ['DM Sans', 'Inter', 'system-ui', 'sans-serif'],
+        sans: ['Inter', 'system-ui', 'sans-serif'],
       },
       borderRadius: {
         'lg': '6px',
